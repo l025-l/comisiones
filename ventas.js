9330dd8801e2e940dcd7100fc845b5af4c445479
l025-l <cristianpegatodo22@gmail.com>
2026-09-24 17:21:59 -0500

    calculo de comision y resultados en pantalla

---
+++ ventas.js
@@ -0,0 +1,42 @@
+const VENTAS_BASE = 5;
+
+function calcularComision(numeroVentas, precioProducto){
+    let comision=0;
+
+    if(numeroVentas > VENTAS_BASE){
+        let ventasExtras=numeroVentas-VENTAS_BASE;
+        comision= ventasExtras*(precioProducto*0.10);
+    }
+
+    return comision
+}
+
+function calcular(){
+    //recuperamos propiedades de las cajas de texto
+    let componenteSueldoBase=document.getElementById("txtSueldoBase");
+    let componenteVentas=document.getElementById("txtVentas");
+    let componentePrecio=document.getElementById("txtPrecio");
+
+    //recuperamos el valor de las cajas de texto
+    let sueldoBaseStr=componenteSueldoBase.value;
+    let numeroVentasStr=componenteVentas.value;
+    let precioProductoStr=componentePrecio.value;
+
+    //convertimos el texto a numero
+    let sueldoBase=parseFloat(sueldoBaseStr);
+    let numeroVentas=parseFloat(numeroVentasStr);
+    let precioProducto=parseFloat(precioProductoStr);
+
+    let comision=calcularComision(numeroVentas, precioProducto);
+
+    let total=sueldoBase+comision;
+
+    let spSueldoBase=document.getElementById("spSueldoBase");
+    let spComision=document.getElementById("spComision");
+    let spTotal=document.getElementById("spTotal");
+
+    spSueldoBase.textContent=sueldoBase;
+    spComision.textContent=comision;
+    spTotal.textContent=total;
+    
+}
